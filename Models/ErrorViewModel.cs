e474eec0707a1b399319a8a2f1338bf38d6452cf
EDUARDO MONTOYA <josma2219@gmail.com>
2026-07-09 19:28:41 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
